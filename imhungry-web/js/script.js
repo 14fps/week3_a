d484b206430d00f73d1b7c5faa152df411921762
14fps <alberlst90s@gmail.com>
2017-09-18 15:37:16 -0700

    display function done

--- imhungry-web/js/script.js
+++ imhungry-web/js/script.js
@@ -1,97 +1,39 @@
-var resArray  =[
-	{
-		name: 'Titos',
-		ctg: 'Ethnic',
-		add:'122 Walter Hardwick Ave 305 Vancouver',
-		src:'img/r1.jpg'
-	},
+var APIURL = "https://imhungry-app.herokuapp.com"
+var target;
 
-	{
-		name: "Hank's Dairy Bar",
-		ctg: 'Fast food',
-		add:'3308 Ash St Vancouver',
-		src:'img/r2.jpg'
-	},
+//get restaurant
+$.ajax({
+	url:APIURL+"/restaurant"
+}).done(function(response){
+	console.log(response);
+	for (var i = 0; i< response.length;i++){
+		console.log(!response[i].image);
+		if(response[i].image&&response[i].name&&response[i].type&&response[i].address){
+			add_res(response[i].homepage,
+					response[i].image,
+					response[i].name,
+					response[i].type,
+					response[i].address)
+		}
+	}
+})
 
-	{
-		name: "D'Wine Restaurant",
-		ctg: "Fast casual",
-		add:'2485 Broadway W 414 Vancouver',
-		src:'img/r3.jpg'
-	},
+init()
 
-	{
-		name: "Basil Doc's Pizza",
-		ctg: "Casual dining",
-		add:'275 28th Ave E Vancouver',
-		src:'img/r4.jpg'
-	},
+function init(){
+	target = $('#resListDiv');
 
-	{
-		name: "Samuel's Memorable",
-		ctg: "Fine dining",
-		add:'106 588 45th Ave W Vancouver',
-		src:'img/r5.jpg'
-	},
+	$('<h1>').text("餐厅列表").addClass('align_center color_grey padding_bottom_25').appendTo(target);
+}
 
-	{
-		name: "Louie's Chophouse",
-		ctg: "Barbeque",
-		add:'563 Union St Vancouver',
-		src:'img/r6.jpg'
-	},
-
-	{
-		name: "Wendal Wendonie",
-		ctg: "Brasserie",
-		add:'3007 8th Ave W Vancouver',
-		src:'img/r7.jpg'
-	},
-
-	{
-		name: "Zodiac Cafe",
-		ctg: "Bistro",
-		add:'110 2255 W 8th Vancouver',
-		src:'img/r8.jpg'
-	},
-
-	{
-		name: "Sweet Addition",
-		ctg: "Buffet",
-		add:'408 2260 W 10th Vancouver',
-		src:'img/r9.jpg'
-	},
-]
-
-
-
-
-/*
-		<div class="rel inline_block padding_bottom_25">
-			<a href="#">
-				<img class="z_neg" src="img/r1.jpg">
-				<div class="abs width_max height_max white_text align_center">
-				
-					<h2 class="abs top_15 abs_align_center">Titos</h2>
-					<h4 class="abs top_50 abs_align_center">Ethnic</h4>
-					<p class="abs top_70 small_text abs_align_center"> 122 Walter Hardwick Ave 305 Vancouver</p>
-
-				</div>
-			</a>
-		</div>
-*/
-var target = $('#resListDiv');
-
-$('<h1>').text("餐厅列表").addClass('align_center color_grey padding_bottom_25').appendTo(target);
-
-for(var i = 0; i< resArray.length;i++){
+function add_res(homepage = "#", img, res_name, res_ctg, res_add){
 	var divBlock=$('<div>').addClass("rel inline_block");
 	var link = $('<a>').attr('href','#');
-	var img = $('<img>').addClass("z_neg").attr("src",resArray[i].src);
+	var img = $('<img>').addClass("z_neg").attr("src",img);
 	var absDiv=$('<div>').addClass("abs width_max height_max white_text align_center");
-	var res_name = $('<h2>').addClass("abs top_15 abs_align_center").text(resArray[i].name);
-	var res_ctg =$('<h4>').addClass("abs top_50 abs_align_center").text(resArray[i].ctg);
-	var res_add =$('<p>').addClass("abs top_70 small_text abs_align_center").text(resArray[i].add);
+	var res_name = $('<h2>').addClass("abs top_15 abs_align_center").text(res_name);
+	var res_ctg =$('<h4>').addClass("abs top_50 abs_align_center").text(res_ctg);
+	var res_add =$('<p>').addClass("abs top_70 small_text abs_align_center").text(res_add);
 
 	res_name.appendTo(absDiv);
 	res_ctg.appendTo(absDiv);
@@ -101,7 +43,6 @@ for(var i = 0; i< resArray.length;i++){
 	link.appendTo(divBlock);
 	divBlock.appendTo(target);	
 }
-
 
 
 
